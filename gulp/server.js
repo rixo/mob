d006b11841b0e31591245517ffd43169708dcf54
rixo <eric@eoko.fr>
2016-03-15 23:29:31 +0100

    Cordova browser sync as a gulp task

---
+++ gulp/server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var _ = require('lodash');
+var path = require('path');
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var $ = require('gulp-load-plugins')();
+
+var cordova = require('cordova-lib').cordova;
+var cordovaBrowserSync = require('cordova-browsersync-primitives');
+var Patcher = require('cordova-plugin-browsersync/lib/utils/Patcher');
+
+gulp.task('serve', function() {
+
+  var projectRoot = path.normalize(__dirname + '/..');
+  var platforms = ['android', 'ios'];
+  var patcher = new Patcher(projectRoot, platforms);
+
+  var bs = cordovaBrowserSync.startBrowserSync(projectRoot, platforms, function(defaults) {
+    defaults.reloadDebounce = 500;
+    defaults.files.push({
+      match: ['www/**/*.*'],
+      fn: function(event, file) {
+        if (event === 'change') {
+          cordova.raw.prepare().then(function() {
+            patcher.addCSP();
+            bs.reload();
+          });
+        }
+      }
+    });
+    return defaults;
+  }, function(err, browserSyncValue) {
+    if (err) {
+      gutil.log("Failed to start browser-sync", err.stack || err);
+    } else {
+      patcher.patch({
+        servers: browserSyncValue.servers
+      });
+    }
+  });
+});
